3de4d1f8bc422e9969766046c4cc8832f50a5362
DESKTOP-5360O1P\server <tkdduq241@naver.com>
2025-02-17 21:21:09 +0900

    프로젝트 첫 생성

---
+++ src/main/java/com/exercise/chatting02/Chatting02Application.java
@@ -0,0 +1,13 @@
+package com.exercise.chatting02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chatting02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chatting02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/exercise/chatting02/Chatting02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.chatting02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chatting02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
