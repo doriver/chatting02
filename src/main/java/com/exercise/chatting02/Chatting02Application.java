27ea22ded7232d032afe18388ea3ad47b3156082
DESKTOP-5360O1P\server <tkdduq241@naver.com>
2025-02-17 23:50:24 +0900

    했던거 옮기는중

--- src/main/java/com/exercise/chatting02/Chatting02Application.java
+++ src/main/java/com/exercise/chatting02/Chatting02Application.java
@@ -2,8 +2,9 @@ package com.exercise.chatting02;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class}) // Spring Security 비활성화
 public class Chatting02Application {
 
 	public static void main(String[] args) {
