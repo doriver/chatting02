289475e80c3f7321f78c096294e69846f17d8a97
DESKTOP-5360O1P\server <tkdduq241@naver.com>
2025-05-26 20:37:00 +0900

    스프링에서 비동기처리 구현 성공, 응답은 바로주고, 백그라운드 스레드에서 따로 실행시킴

--- src/main/java/com/exercise/chatting02/Chatting02Application.java
+++ src/main/java/com/exercise/chatting02/Chatting02Application.java
@@ -3,7 +3,9 @@ package com.exercise.chatting02;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
 @SpringBootApplication// (exclude = {SecurityAutoConfiguration.class}) Spring Security 비활성화
 public class Chatting02Application {
 
